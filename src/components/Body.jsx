4d9e0a9abffbc4dfecaa871e7abf1d782e347920
Shubham patil <shubham.patil@sincrodigital.com>
2023-12-20 12:33:53 +0530

    added a API call , useEffect hook and shimmer UI

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,34 @@
 
 import RestaurantCard from "./RestaurantCard";  
 import resList from "../utils/mockdata";
-import React, { useState } from "react"; 
+import React, { useState, useEffect } from "react"; 
+import Shimmer from "./Shimmer";
 
 const BodyComponent = () => {
-    const [RestaurantList, setFilteredRestaurantList] = useState(resList)
-   
+    const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
+    const [RestaurantList, setRestaurantList] = useState([])
+    useEffect(()=> {
+      fetchData();
+    },
+    []);
+
+    const fetchData = async () => {
+      const response = await fetch(SWIGGY_API);
+      const data = await response.json();
+      let apiResData = data?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      setRestaurantList(apiResData);
+    }
+
+    if(RestaurantList.length === 0) {
+      return <Shimmer/>
+    }
+
     return (
       <div className="body">
           <div className="filter">
             <button className="filter-btn" onClick={() => {
                const filteredList = RestaurantList.filter((item) => item.info.avgRating > 4);
-               setFilteredRestaurantList(filteredList); 
+               setRestaurantList(filteredList); 
             }}>Top Rated Restaurants </button>
           </div>
           <div className="res-container">
