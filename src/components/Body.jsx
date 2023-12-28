5319ef9e72915552d0adc4cec45c03d2de485331
Shubham patil <shubham.patil@sincrodigital.com>
2023-12-28 18:16:31 +0530

    code refactored

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,86 +1,96 @@
-
-import RestaurantCard from "./RestaurantCard";  
-import React, { useState } from "react"; 
+import RestaurantCard from "./RestaurantCard";
+import React, { useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 
 import useRestaurantList from "../utils/useRestaurantList";
 import useOnlineStatus from "../utils/useOnlineStatus";
 const BodyComponent = () => {
-    
+  // Whenever the state variables update,  React triggers the reconciliation cycle ( re-renders the components)
 
-    // Whenever the state variables update,  React triggers the reconciliation cycle ( re-renders the components) 
-    
-    const [searchText, setSearchText] = useState("");
-    
-    const { RestaurantList, FilteredListOfRestaurants, setFilteredListOfRestaurants, isLoading } = useRestaurantList();
-    // no dependency array it is called on every component render
-    // empty Dependency array -> [] -> it will be called only on initial render, so here passing [] as I want the API call on page load only ( just once )
+  const [searchText, setSearchText] = useState("");
 
-    // Conditional rendering
-    const onlineStatus = useOnlineStatus();
+  const {
+    RestaurantList,
+    FilteredListOfRestaurants,
+    setFilteredListOfRestaurants,
+    isLoading,
+  } = useRestaurantList();
+  // no dependency array it is called on every component render
+  // empty Dependency array -> [] -> it will be called only on initial render, so here passing [] as I want the API call on page load only ( just once )
 
-    if(!onlineStatus) {
-      return <h1>looks like you are Offline, please check internet connection...  </h1>
-    }
+  // Conditional rendering
+  const onlineStatus = useOnlineStatus();
 
-    if(isLoading) {
-      return <Shimmer/>
-    }
-
+  if (!onlineStatus) {
     return (
-      <div className="body">
-          <div className="filter flex flex-col md:flex-row md:items-center p-4 md:p-0">
-  <div className="search mb-4 md:mb-0 md:mr-4 flex items-center">
-    <input
-      type="text"
-      className="search-box border border-solid border-black px-4 py-2 rounded"
-      placeholder="Search Restaurant"
-      value={searchText}
-      onChange={(e) => {
-        setSearchText(e.target.value);
-      }}
-    />
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded"
-      onClick={() => {
-        const filteredRestaurants = RestaurantList.filter((res) =>
-          res.info.name.toLowerCase().includes(searchText.toLowerCase())
-        );
-        setFilteredListOfRestaurants(filteredRestaurants);
-      }}
-    >
-      Search
-    </button>
-  </div>
-  
-  <div className="flex">
-    <button
-      className="filter-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={() => {
-        const filteredList = FilteredListOfRestaurants.filter(
-          (item) => item.info.avgRating > 4
-        );
-        setFilteredListOfRestaurants(filteredList);
-      }}
-    >
-      Top Rated Restaurants
-    </button>
-  </div>
-</div>
-
-
-          <div className="res-container flex flex-auto flex-wrap">
-              {    
-                   FilteredListOfRestaurants && FilteredListOfRestaurants.length > 0 ? (FilteredListOfRestaurants.map((restaurant) => {
-                      return <Link key={restaurant.info.id} to={`/restaurant/${restaurant.info.id}`}><RestaurantCard resData = {restaurant}/></Link>
-                   }) ) : (
-                     <p>Zero Results Found...</p>
-                   )                      
-              } 
-          </div>
-      </div>
-    )
+      <h1>looks like you are Offline, please check internet connection... </h1>
+    );
   }
 
-  export default BodyComponent;
+  if (isLoading) {
+    return <Shimmer />;
+  }
+
+  return (
+    <div className="body">
+      <div className="filter flex flex-col md:flex-row md:items-center p-4 md:p-0">
+        <div className="search mb-4 md:mb-0 md:mr-4 flex items-center">
+          <input
+            type="text"
+            className="search-box border border-solid border-black px-4 py-2 rounded"
+            placeholder="Search Restaurant"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded"
+            onClick={() => {
+              const filteredRestaurants = RestaurantList.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredListOfRestaurants(filteredRestaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
+        <div className="flex">
+          <button
+            className="filter-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              const filteredList = FilteredListOfRestaurants.filter(
+                (item) => item.info.avgRating > 4
+              );
+              setFilteredListOfRestaurants(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+      </div>
+
+      <div className="res-container flex flex-auto flex-wrap">
+        {FilteredListOfRestaurants && FilteredListOfRestaurants.length > 0 ? (
+          FilteredListOfRestaurants.map((restaurant) => {
+            return (
+              <Link
+                key={restaurant.info.id}
+                to={`/restaurant/${restaurant.info.id}`}
+              >
+                <RestaurantCard resData={restaurant} />
+              </Link>
+            );
+          })
+        ) : (
+          <p>Zero Results Found...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BodyComponent;
